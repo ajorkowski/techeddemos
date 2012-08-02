2548780f0ec8eed4701ee36a9b1c0c51980467fd
Felix Jorkowski <a.jorkowski@gmail.com>
2012-08-02 16:47:14 +1000

    Completed End project for first demo

--- TechEdExample1End/Demo.Web/Controllers/AccountController.cs
+++ TechEdExample1End/Demo.Web/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using Demo.Web.Models;
 using Demo.Web.Code;
 using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using DotNetOpenAuth.AspNet.Clients;
 
 namespace Demo.Web.Controllers
 {
@@ -49,6 +52,48 @@ namespace Demo.Web.Controllers
             else
             {
                 return View(new LoginModel());
+            }
+        }
+
+        //
+        // GET: /Account/Google
+        public ActionResult Google()
+        {
+            var googleClient = new GoogleOpenIdClient();
+            return Authenticate(googleClient);
+        }
+
+        //
+        // GET: /Account/Live
+        public ActionResult Live()
+        {
+            var liveClient = new MicrosoftClient("appId", "appSecret");
+            return Authenticate(liveClient);
+        }
+
+        // GET: /Accounts/Facebook
+        public ActionResult Facebook()
+        {
+            var facebookClient = new FacebookClient("appId", "appSecret");
+            return Authenticate(facebookClient);
+        }
+
+        private ActionResult Authenticate(IAuthenticationClient client)
+        {
+            try
+            {
+                var auth = client.VerifyAuthentication(HttpContext);
+                if (auth.IsSuccessful)
+                {
+                    FormsAuthentication.SetAuthCookie(auth.UserName, false);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            catch (InvalidOperationException)
+            {
+                // If the verification failed we have to redirect
+                client.RequestAuthentication(HttpContext, Request.Url);
+                return null;
             }
         }
 
